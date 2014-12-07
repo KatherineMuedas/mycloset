5d7c4db3d2db3de3b78ecbe6b8453626e271cfa4
katherine <muedasnka@gmail.com>
2014-12-07 18:52:29 -0500

    adds closet controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'closets/show'
+
+  get 'closets/new'
+
+  get 'closets/create'
+
+  get 'closets/edit'
+
+  get 'closets/update'
+
+  get 'closets/destroy'
+
   root to: 'visitors#index'
   devise_for :users
   resources :users
