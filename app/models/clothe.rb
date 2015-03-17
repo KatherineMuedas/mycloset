fc79cb50f3525fb74f3e32d6f4cde1c2afb5274e
katherine <muedasnka@gmail.com>
2015-03-17 15:38:41 -0600

    adds padding to new clothe form and remove footer

--- app/models/clothe.rb
+++ app/models/clothe.rb
@@ -1,6 +1,11 @@
 class Clothe < ActiveRecord::Base
   belongs_to :closet
   belongs_to :user
+  # validates :name, :purchase_at, :brand, :type_category, presence: true
   has_one :picture, as: :picable, dependent: :destroy
+
   accepts_nested_attributes_for :picture
+
+  
+  
 end
